bd2ef97f338b596224cf0810e15b40a9b6566caf
Kisna28 <kisnapatel9909@gmail.com>
2023-02-22 22:45:56 +0530

    Mainactivity

--- app/src/main/java/com/example/constrain_layout/MainActivity.java
+++ app/src/main/java/com/example/constrain_layout/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.constrain_layout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.constrain_layout.viewmodels.ViewModelData;
 
+public class MainActivity extends AppCompatActivity {
+ViewModelData viewModelData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
